7c31c360d21bd33031aba9d8d95f57de106d1711
ancyelove <aneye_love@126.com>
2013-09-06 20:44:27 -0700

    扩展webview

---
+++ src/com/zuiniu/android/assistant/view/CustomWebView.java
@@ -0,0 +1,18 @@
+package com.zuiniu.android.assistant.view;
+
+import android.content.Context;
+import android.webkit.WebView;
+
+/**
+ * 
+ * @author ancye.Xie
+ *
+ */
+public class CustomWebView extends WebView {
+
+	public CustomWebView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
